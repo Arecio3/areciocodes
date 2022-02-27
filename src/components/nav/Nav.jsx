99e453905e9692440a1843003c8adb7bbb8c6146
Arecio Canton <devmain@Arecios-iMac.local>
2022-02-27 02:40:03 -0500

    Added nav icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import { AiFillHome } from 'react-icons/ai'
+import { MdWork } from 'react-icons/md'
+import { BiMessageRounded } from 'react-icons/bi'
+import { FaUserAstronaut } from 'react-icons/fa'
+import { BsUiChecks } from 'react-icons/bs'
+import { HiCode } from 'react-icons/hi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiFillHome /></a>
+      <a href="#about"><FaUserAstronaut /></a>
+      <a href="#experience"><HiCode /></a>
+      <a href="#services"><MdWork /></a>
+      <a href="#reviews"><BsUiChecks /></a>
+      <a href="#contact"><BiMessageRounded/></a>
+    </nav>
   )
 }
 
