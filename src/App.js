d4191ae3d0b234ad22a6a941917182b4bb523d94
Arecio Canton <devmain@Arecios-iMac.local>
2022-02-26 23:54:44 -0500

    Added general styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about';
+import Contact from './components/contact';
+import Experience from './components/experience';
+import Footer from './components/footer';
+import Header from './components/header';
+import Nav from './components/nav';
+import Portfolio from './components/portfolio';
+import Reviews from './components/reviews';
+import Services from './components/services';
 
 function App() {
   return (
-    <div>
-     Hey
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Reviews />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
