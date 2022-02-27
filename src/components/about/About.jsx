49f2886da9ede6d703c2b54d4adc1223c41562c4
Arecio Canton <devmain@Arecios-iMac.local>
2022-02-27 04:19:49 -0500

    added cool animation in about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/meWithABack.JPG";
+import { FaAward } from "react-icons/fa";
+import { ImUsers } from "react-icons/im";
+import { GiAtom } from "react-icons/gi";
 
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon"/>
+              <h5>Experience</h5>
+              <small>2 Years Coding</small>
+            </article>
+            <article className="about__card">
+              <ImUsers className="about__icon"/>
+              <h5>Clients</h5>
+              <small>5 Clients</small>
+            </article>
+            <article className="about__card">
+              <GiAtom className="about__icon"/>
+              <h5>Projects</h5>
+              <small>13 Projects Completed</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam consectetur odit deleniti natus possimus, blanditiis vel fuga officia voluptatum sequi porro ut reiciendis est dignissimos deserunt expedita, magnam amet perspiciatis.</p>
+          <a href="#contact" className="btn btn-primary">Contact</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
