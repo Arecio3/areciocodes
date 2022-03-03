0d450988df637c743acc66d954b4610b5ba6910a
Arecio Canton <devmain@Arecios-iMac.local>
2022-03-02 20:09:31 -0500

    moving portoflio data to seperate file

---
+++ src/data.js
@@ -0,0 +1,50 @@
+exports.data = [
+    {
+        id: 1,
+        image: 1,
+        title: "Hummingbird",
+        subtitle: "Landscaping Business",
+        github: "https://github.com/Arecio3/Humming_bird",
+        website:"https://hummingbirdlawn.com/" 
+    },
+    {
+        id: 2,
+        image: 2,
+        title: "Value Haulers",
+        subtitle: "Tricking Business",
+        github: "https://github.com/Arecio3/Humming_bird",
+        website:"https://hummingbirdlawn.com/" 
+    },
+    {
+        id: 3,
+        image: 3,
+        title: "Popnado",
+        subtitle: "Landscaping Business",
+        github: "https://github.com/Arecio3/Humming_bird",
+        website:"https://hummingbirdlawn.com/" 
+    },
+    {
+        id: 4,
+        image: 4,
+        title: "Halo",
+        subtitle: "Landscaping Business",
+        github: "https://github.com/Arecio3/Humming_bird",
+        website:"https://hummingbirdlawn.com/" 
+    },
+    {
+        id: 5,
+        image: 5,
+        title: "Searchy",
+        subtitle: "Landscaping Business",
+        github: "https://github.com/Arecio3/Humming_bird",
+        website:"https://hummingbirdlawn.com/" 
+    },
+    {
+        id: 6,
+        image: 6,
+        title: "Documaker",
+        subtitle: "Landscaping Business",
+        github: "https://github.com/Arecio3/Humming_bird",
+        website:"https://hummingbirdlawn.com/" 
+    },
+]
